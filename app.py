d3379ca360eca987e6fbccd894d74b4f85bb7ff4
星雲希凪 <70424266+MejiroRina@users.noreply.github.com>
2025-03-24 10:00:30 +0800

    [WIP] Setup app

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from quart import Quart
+from quart import Quart, request, jsonify
 
 from api import apis
+from configs.app import ACCPET_AUTHORIZATION, ACCEPT_USER_AGENT
 
 app = Quart(__name__)
 for api in apis:
     app.register_blueprint(api)
+
+
+@app.before_request
+async def check_authorization():
+    if ACCPET_AUTHORIZATION and request.headers.get('Authorization') != ACCPET_AUTHORIZATION:
+        return jsonify({'error': 'Unauthorized'}), 401
+    if ACCEPT_USER_AGENT and request.headers.get('User-Agent') != ACCEPT_USER_AGENT:
+        return jsonify({'error': 'Invalid User Agent'}), 400
---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+import asyncio
+import logging
+import platform
+import coloredlogs
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
+
+from app import app
+from configs.app import LOG_FORMAT, FIELD_STYLE, HOST, PORT
+
+
+async def run() -> None:
+    config = Config()
+    config.bind = [f"{HOST}:{PORT}"]
+    config.loglevel = "info"
+    config.access_log_format = "%(h)s %(r)s %(s)s %(b)s %(M)s"
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    coloredlogs.install(level="INFO", logger=logger, fmt=LOG_FORMAT, field_style=FIELD_STYLE)
+    app.logger.setLevel(logging.INFO)
+    await serve(app, config)
+
+
+if __name__ == '__main__':
+    if platform.system() == 'Windows':
+        asyncio.run(run())
+    else:
+        import uvloop
+
+        python_version = sys.version_info
+        if python_version >= (3, 11):
+            uvloop.run(run())
+        else:
+            uvloop.install()
+            asyncio.run(run())
